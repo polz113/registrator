9868067f8782fd82453f47795990157db2c51dfd
polz <polz@fri.uni-lj.si>
2025-07-30 15:36:20 +0200

    Prazen seznam dogodkov naj ne bo razlog za samomor programa

--- php/eventLists.php
+++ php/eventLists.php
@@ -1,8 +1,8 @@
 <?PHP
     $user = $_SESSION['employeeID'];
     $old_events = file("spool/".basename($user)."/old_events.csv");# or die("Unable to open file!");
-    $old_events = array_reverse($old_events);
+    $old_events = $old_events ? array_reverse($old_events) : [];
     $new_events = file("spool/".basename($user)."/new_events.csv");# or die("Unable to open file!");
-    $new_events = array_reverse($new_events);
+    $new_events = $new_events ? array_reverse($new_events): [];
 
 ?>
